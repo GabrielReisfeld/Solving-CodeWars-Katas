6f9fd2a0882e6089ce2565a57063ee45a6da4740
Gabriel Reisfeld <greisfeld@gmail.com>
2021-05-02 12:48:12 -0300

    Solved "101 Dalmatians - squash the bugs, not"

--- 8kyu/101 Dalmatians - squash the bugs, not the dogs!.js
+++ 8kyu/101 Dalmatians - squash the bugs, not the dogs!.js
@@ -7,3 +7,8 @@ The number of dogs will always be a number and there will always be at least 1 d
 
 Good luck!
 */
+
+function howManyDalmatians(number) {
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+    return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number === 101 ? dogs[3] : dogs[2];
+  }
