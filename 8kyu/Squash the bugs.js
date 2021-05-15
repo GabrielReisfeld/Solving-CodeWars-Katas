95b87a1760f7915e3baec524937c300fda2aae0e
Gabriel Reisfeld <greisfeld@gmail.com>
2021-05-15 13:41:42 -0300

    Solved "Squash the bugs"

--- 8kyu/Squash the bugs.js
+++ 8kyu/Squash the bugs.js
@@ -16,3 +16,15 @@ function findLongest(str) (
     return longest
 )
 */
+
+function findLongest(str) {
+  var spl = str.split(" ");
+  var longest = 0;
+
+  for (var i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest) {
+      longest = spl[i].length;
+    }
+  }
+  return longest;
+}
