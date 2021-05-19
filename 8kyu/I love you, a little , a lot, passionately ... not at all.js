21e9ca1fdc40b6445f936e8d9e00924ce4e81b4c
Gabriel Reisfeld <greisfeld@gmail.com>
2021-05-19 13:26:23 -0300

    Solved "I love you, a little , a lot, passionatel"

--- 8kyu/I love you, a little , a lot, passionately ... not at all.js
+++ 8kyu/I love you, a little , a lot, passionately ... not at all.js
@@ -12,3 +12,14 @@ When the last petal was torn there were cries of excitement, dreams, surging tho
 
 Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
 */
+
+const phrases = [
+  "not at all",
+  "I love you",
+  "a little",
+  "a lot",
+  "passionately",
+  "madly",
+];
+
+const howMuchILoveYou = (n) => phrases[n % 6];
