50de4f063b3b823c560a35eb93cba3710448b7f0
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-06 23:27:30 -0300

    Solved "Find numbers which are divisible by given"

--- Find numbers which are divisible by given number.js
+++ Find numbers which are divisible by given number.js
@@ -4,3 +4,9 @@ Example
 
 divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
 */
+
+function divisibleBy(numbers, divisor) {
+  var res = [];
+  numbers.map((x) => (x % divisor === 0 ? res.push(x) : x));
+  return res;
+}
