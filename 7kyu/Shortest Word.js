2f30e38ff0972ce8f50905decc2a6fab092020e8
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-15 13:10:03 -0300

    Solved "Shortest Word"

--- 7kyu/Shortest Word.js
+++ 7kyu/Shortest Word.js
@@ -3,3 +3,10 @@ Simple, given a string of words, return the length of the shortest word(s).
 
 String will never be empty and you do not need to account for different data types.
 */
+
+function findShort(s) {
+  return s.split(" ").reduce((a, b) => (b.length > a.length ? a : b)).length;
+  /*var array = s.split(' ');
+    var min = array.reduce((a, b) => (b.length > a.length) ? a : b)
+    return min.length;*/
+}
