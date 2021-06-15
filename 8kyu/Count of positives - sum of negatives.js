245b3ffcf68033c638ee88f0967260c3b8888244
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-15 12:17:26 -0300

    Solved "Count of positives / sum of negatives"

--- 8kyu/Count of positives - sum of negatives.js
+++ 8kyu/Count of positives - sum of negatives.js
@@ -8,3 +8,20 @@ Example
 
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 */
+
+function countPositivesSumNegatives(input) {
+  if (input === null || input.length === 0) {
+    return [];
+  }
+  var countPositives = 0;
+  var sumNegatives = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      countPositives += 1;
+    } else {
+      sumNegatives += input[i];
+    }
+  }
+  return [countPositives, sumNegatives];
+}
