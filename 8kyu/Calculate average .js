bef9c524f243f248cc0f127437d61f3461a7559b
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-16 15:08:10 -0300

    Solved "Calculate average"

--- 8kyu/Calculate average .js
+++ 8kyu/Calculate average .js
@@ -3,3 +3,7 @@ Write a function which calculates the average of the numbers in a given list.
 
 Note: Empty arrays should return 0.
 */
+
+function find_average(array) {
+  return array.reduce((acc, curr) => acc + curr) / array.length;
+}
