f5e620961b17332a48b7579051e080518d0fe299
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-17 08:39:28 -0300

    Solved "Are You Playing Banjo?"

--- 8kyu/Are You Playing Banjo.js
+++ 8kyu/Are You Playing Banjo.js
@@ -9,3 +9,9 @@ name + " does not play banjo"
 
 Names given are always valid strings.
 */
+
+function areYouPlayingBanjo(name) {
+  return name.charAt(0) === "R" || name.charAt(0) === "r"
+    ? name + " plays banjo"
+    : name + " does not play banjo";
+}
