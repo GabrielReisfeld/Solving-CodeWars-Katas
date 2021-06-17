ce3c20b41c5db910bf950b411c3593b56ff51f9c
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-17 08:36:22 -0300

    Solved "Array plus array"

--- 8kyu/Array plus array.js
+++ 8kyu/Array plus array.js
@@ -6,4 +6,5 @@ P.S. Each array includes only integer numbers. Output is a number too.
 
 function arrayPlusArray(arr1, arr2) {
     return arr1.reduce((acc, curr) => acc + curr, 0) + arr2.reduce((acc, curr) => acc + curr, 0);
+    // return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
   }
