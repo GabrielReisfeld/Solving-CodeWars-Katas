6ad1f779e5976d72692219cf74cb0a1ec75f3912
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-17 08:55:43 -0300

    Solved "Reversed sequence"

--- 8kyu/Reversed sequence.js
+++ 8kyu/Reversed sequence.js
@@ -3,3 +3,11 @@ Build a function that returns an array of integers from n to 1 where n>0.
 
 Example : n=5 --> [5,4,3,2,1]
 */
+
+const reverseSeq = (n) => {
+  var res = [];
+  for (let i = n; i > 0; i--) {
+    res.push(i);
+  }
+  return res;
+};
