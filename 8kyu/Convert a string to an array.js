26dbbdabbb993ea8307f062a33a93f1014df5c1c
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-21 00:55:27 -0300

    Solved "Convert a string to an array"

--- 8kyu/Convert a string to an array.js
+++ 8kyu/Convert a string to an array.js
@@ -5,3 +5,7 @@ Write a function to split a string and convert it into an array of words. For ex
 
 "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 */
+
+function stringToArray(string) {
+  return string.split(" ");
+}
