5d64e06c94703f768099d295b82120644b0c1548
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-22 09:56:39 -0300

    Solved "Rock Paper Scissors!"

--- 8kyu/Rock Paper Scissors!.js
+++ 8kyu/Rock Paper Scissors!.js
@@ -9,3 +9,11 @@ rps('scissors','paper') // Player 1 won!
 rps('scissors','rock') // Player 2 won!
 rps('paper','paper') // Draw!
 */
+
+const rps = (p1, p2) => {
+  if (p1 === "scissors" && p2 === "paper") return "Player 1 won!";
+  if (p1 === "rock" && p2 === "scissors") return "Player 1 won!";
+  if (p1 === "paper" && p2 === "rock") return "Player 1 won!";
+  if (p1 === p2) return "Draw!";
+  return "Player 2 won!";
+};
