405d6bdbcef9a28669f97566e576f9ec87e4eee2
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-22 09:50:42 -0300

    Solved "Unfinished Loop - Bug Fixing #1"

--- 8kyu/Unfinished Loop - Bug Fixing #1.js
+++ 8kyu/Unfinished Loop - Bug Fixing #1.js
@@ -14,3 +14,13 @@ function createArray(number){
 }
 
 */
+
+function createArray(number) {
+  var newArray = [];
+
+  for (var counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
