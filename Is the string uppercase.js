250c2e520058c637f35d05e3be1a8333f8ea7602
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-22 23:47:57 -0300

    Solved "Is the string uppercase?"

--- Is the string uppercase.js
+++ Is the string uppercase.js
@@ -13,3 +13,7 @@ Add the isUpperCase method to String to see whether the string is ALL CAPS. For 
 
 In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 */
+
+String.prototype.isUpperCase = function () {
+  return this.toString() === this.toUpperCase();
+};
