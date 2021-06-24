d78db3c75710ac064b65796c645c97f5e5408983
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-24 01:28:15 -0300

    Solved "Credit Card Mask"

--- 7kyu/Credit Card Mask.js
+++ 7kyu/Credit Card Mask.js
@@ -16,4 +16,6 @@ maskify("Nananananananananananananananana Batman!") == "########################
 
 function maskify(cc) {
   return cc.slice(0, -4).replace(/./g, "#") + cc.slice(-4);
+  // Another Solution:
+  // return cc.replace(/.(?=.{4})/g, '#');
 }
