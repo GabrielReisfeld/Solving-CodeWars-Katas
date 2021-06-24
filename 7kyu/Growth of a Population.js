b71ef2eeb844e60686638654e855c1636167618e
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-24 01:37:36 -0300

    Solved "Growth of a Population"

--- 7kyu/Growth of a Population.js
+++ 7kyu/Growth of a Population.js
@@ -28,3 +28,12 @@ Note:
 
 Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
 */
+
+function nbYear(p0, percent, aug, p) {
+  var years = 0;
+  while (p0 < p) {
+    p0 = p0 + p0 * (percent / 100) + aug;
+    years++;
+  }
+  return years;
+}
