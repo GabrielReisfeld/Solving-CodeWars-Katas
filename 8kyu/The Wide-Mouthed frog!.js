7827ae6a76292223591b38e4f55a6246605005d8
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-25 13:01:07 -0300

    Solved "The Wide-Mouthed frog!"

--- 8kyu/The Wide-Mouthed frog!.js
+++ 8kyu/The Wide-Mouthed frog!.js
@@ -10,4 +10,6 @@ Your goal in this kata is to create complete the mouth_size method this method t
 
 function mouthSize(animal) {
   return animal.toLowerCase() === "alligator" ? "small" : "wide";
+  // Another Solution:
+  // return animal.match(/alligator/i) ? 'small' : 'wide';
 }
