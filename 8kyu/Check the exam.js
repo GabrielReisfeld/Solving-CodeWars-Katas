97f6a2afed37ca45e541afba7d3377e2052d2512
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-26 20:11:53 -0300

    Solved "Check the exam"

--- 8kyu/Check the exam.js
+++ 8kyu/Check the exam.js
@@ -26,3 +26,21 @@ function checkExam(array1, array2) {
   });
   return points > 0 ? points : 0;
 }
+// Another Solution:
+// function checkExam(array1, array2) {
+//     var score = 0;
+//       for (var i = 0; i < array2.length; i++){
+//           if (array2[i] === ""){
+//              score += 0
+//           } else if (array1[i] === array2[i]){
+//               score += 4
+//           } else if (array1[i] !== array2[i]){
+//              score -= 1
+//           }
+//       }
+//         if (score >= 0){
+//           return score;
+//         } else {
+//           return 0
+//         }
+//    }
