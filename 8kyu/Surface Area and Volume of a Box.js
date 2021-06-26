2c4aae07f22fae6ea9c68630653d23df3299e04a
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-26 18:53:14 -0300

    Solved "Surface Area and Volume of a Box"

--- 8kyu/Surface Area and Volume of a Box.js
+++ 8kyu/Surface Area and Volume of a Box.js
@@ -1,3 +1,9 @@
 /*
 Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 */
+
+function getSize(width, height, depth) {
+  const area = (depth * width + depth * height + width * height) * 2;
+  const volume = width * height * depth;
+  return [area, volume];
+}
