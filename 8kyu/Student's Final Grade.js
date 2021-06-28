fa33303d900f616894295d04dfbe16af59881b60
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-27 23:20:27 -0300

    Solved "Student's Final Grade"

--- 8kyu/Student's Final Grade.js
+++ 8kyu/Student's Final Grade.js
@@ -25,3 +25,10 @@ Examples(Inputs-->Output):
 
 *Use Comparison and Logical Operators.
 */
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) return 100;
+  if (exam > 75 && projects >= 5) return 90;
+  if (exam > 50 && projects >= 2) return 75;
+  return 0;
+}
