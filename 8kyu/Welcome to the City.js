c4e39246163f759f36730a16d9d6f6187f90c17f
Gabriel Reisfeld <greisfeld@gmail.com>
2021-06-30 11:20:39 -0300

    Solved "Welcome to the City"

--- 8kyu/Welcome to the City.js
+++ 8kyu/Welcome to the City.js
@@ -7,3 +7,7 @@ sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
 
 This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
 */
+
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
+}
