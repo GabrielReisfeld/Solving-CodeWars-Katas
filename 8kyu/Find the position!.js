253e41547ff5cb1d5429c4ea4a3f042206808adb
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-02 11:19:25 -0300

    Solved "Find the position!"

--- 8kyu/Find the position!.js
+++ 8kyu/Find the position!.js
@@ -5,3 +5,8 @@ Input :: "a"
 
 Ouput :: "Position of alphabet: 1"
 */
+
+function position(letter){
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    return `Position of alphabet: ${alphabet.findIndex(l => l === letter) + 1}`;
+  }
