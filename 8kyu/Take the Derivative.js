25271a9917a282661b61209b00e0ecf28041eedd
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-04 17:42:24 -0300

    Solved "Take the Derivative"

--- 8kyu/Take the Derivative.js
+++ 8kyu/Take the Derivative.js
@@ -17,3 +17,7 @@ Notes:
     The output of this function should be a string
     The exponent will never be 1, and neither number will ever be 0
 */
+
+function derive(coefficient, exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`;
+}
