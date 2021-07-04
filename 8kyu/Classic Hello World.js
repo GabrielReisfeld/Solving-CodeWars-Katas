12086bc3181f61967c87c3ddabbd87fc0fc1f5d6
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-04 17:46:41 -0300

    Solved "Classic Hello World"

--- 8kyu/Classic Hello World.js
+++ 8kyu/Classic Hello World.js
@@ -14,3 +14,9 @@ Hints:
     For prolog you can use write but there are better ways
     If you still don't get it probably you can define main as an attribute of the Solution class that accepts a single argument, and that only prints "Hello World!" without any return.
 */
+
+class Solution {
+  static main() {
+    console.log("Hello World!");
+  }
+}
