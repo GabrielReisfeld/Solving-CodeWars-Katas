d41ea6682409229e40c1b814d39881b4b235c291
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-05 11:01:13 -0300

    Solved "Return the Day"

--- 8kyu/Return the day.js
+++ 8kyu/Return the day.js
@@ -10,3 +10,24 @@ Complete the function which returns the weekday according to the input number:
     7 returns "Saturday"
     Otherwise returns "Wrong, please enter a number between 1 and 7"
 */
+
+function whatday(num) {
+  switch (num) {
+    case 1:
+      return "Sunday";
+    case 2:
+      return "Monday";
+    case 3:
+      return "Tuesday";
+    case 4:
+      return "Wednesday";
+    case 5:
+      return "Thursday";
+    case 6:
+      return "Friday";
+    case 7:
+      return "Saturday";
+    default:
+      return "Wrong, please enter a number between 1 and 7";
+  }
+}
