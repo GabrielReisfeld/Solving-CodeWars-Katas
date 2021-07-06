bd7d976b36509866b4a45d596d9baa0001ca3215
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-06 11:27:05 -0300

    Solved "Grader"

--- 8kyu/Grader.js
+++ 8kyu/Grader.js
@@ -16,3 +16,11 @@ grader(0.8) should be "B"
 grader(0.7) should be "C"
 grader(0.6) should be "D"
 */
+
+function grader(score) {
+  if (score > 1 || score < 0.6) return "F";
+  if (score >= 0.9) return "A";
+  if (score >= 0.8) return "B";
+  if (score >= 0.7) return "C";
+  return "D";
+}
