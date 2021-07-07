529ed33f9234c2d5907a28450923ed7794ca7282
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-07 12:11:43 -0300

    Solved "Training JS #4: Basic data types--Array"

--- 8kyu/Training JS #4 Basic data types--Array.js
+++ 8kyu/Training JS #4 Basic data types--Array.js
@@ -25,3 +25,27 @@ I've written five function, each function receives a parameter: arr(an array), y
     4. pushElement(arr)  should push an element to arr, and then return arr
     5. popElement(arr)   should pop an element from arr, and then return arr
 */
+
+function getLength(arr) {
+  //return length of arr
+  return arr.length;
+}
+function getFirst(arr) {
+  //return the first element of arr
+  return arr[0];
+}
+function getLast(arr) {
+  //return the last element of arr
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  var el = 1;
+  //push el to arr
+  arr.push(el);
+  return arr;
+}
+function popElement(arr) {
+  //pop an element from arr
+  arr.pop();
+  return arr;
+}
