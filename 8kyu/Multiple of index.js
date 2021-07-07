8f1de3975a613bfa31ab4f00bbcefa80261b2cb5
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-07 12:06:51 -0300

    Solved "Multiple of index"

--- 8kyu/Multiple of index.js
+++ 8kyu/Multiple of index.js
@@ -10,3 +10,11 @@ Some cases:
 [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
 
 */
+
+function multipleOfIndex(array) {
+  var res = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % i === 0) res.push(array[i]);
+  }
+  return res;
+}
