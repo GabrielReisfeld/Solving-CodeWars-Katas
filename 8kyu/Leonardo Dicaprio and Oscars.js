aaf0866202520080ae35bf523006558d2eb8af6a
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-08 13:30:59 -0300

    Solved "Leonardo Dicaprio and Oscars"

--- 8kyu/Leonardo Dicaprio and Oscars.js
+++ 8kyu/Leonardo Dicaprio and Oscars.js
@@ -15,4 +15,10 @@ function leo(oscar) {
   if (oscar === 86) return "Not even for Wolf of wallstreet?!";
   if (oscar < 88) return "When will you give Leo an Oscar?";
   return "Leo got one already!";
+
+  // Another Solution:
+  // return oscar === 88 ? 'Leo finally won the oscar! Leo is happy' :
+  // oscar === 86 ? 'Not even for Wolf of wallstreet?!' :
+  // oscar  <  88 ? 'When will you give Leo an Oscar?' :
+  // 'Leo got one already!';
 }
