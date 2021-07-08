d3a962c4e43ec6a14f5c5bec37f25059fe0826c7
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-08 13:51:19 -0300

    Solved "simple calculator"

--- 8kyu/simple calculator.js
+++ 8kyu/simple calculator.js
@@ -14,3 +14,19 @@ calculator(1,"k","*"); //=> result will be "unknown value"
 
 Good luck!
 */
+
+function calculator(a, b, sign) {
+  if (typeof a !== "number" || typeof b !== "number") return "unknown value";
+  switch (sign) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    default:
+      return "unknown value";
+  }
+}
