d7e77de67a5e140c2b84f8b9572d6ce51310739e
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-08 13:27:37 -0300

    Solved "Barking mad"

--- 8kyu/Barking mad.js
+++ 8kyu/Barking mad.js
@@ -6,3 +6,15 @@ scoobydoo.bark(); // undefined
 
 Use method prototypes to enable all Dogs to bark.
 */
+
+function Dog(breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+Dog.prototype.bark = function () {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
