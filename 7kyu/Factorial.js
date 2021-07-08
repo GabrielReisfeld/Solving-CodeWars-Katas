43760aee6cfc9bc0ce085c995c7bc8b59a38a820
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-08 14:10:44 -0300

    Solved "Factorial"

--- 7kyu/Factorial.js
+++ 7kyu/Factorial.js
@@ -3,3 +3,11 @@ In mathematics, the factorial of a non-negative integer n, denoted by n!, is the
 
 Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
 */
+
+function factorial(n) {
+  if (n < 0 || n > 12) throw new RangeError();
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
