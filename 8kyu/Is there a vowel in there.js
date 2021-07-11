44689f0d9b8e799d785bc25649d85464f68ffeec
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-11 12:44:11 -0300

    Solved "Is there a vowel in there?"

--- 8kyu/Is there a vowel in there.js
+++ 8kyu/Is there a vowel in there.js
@@ -5,3 +5,11 @@ If they are, change the array value to a string of that vowel.
 
 Return the resulting array.
 */
+
+function isVow(a) {
+  return a.map((charCode) =>
+    /[aeiou]/.test(String.fromCharCode(charCode))
+      ? String.fromCharCode(charCode)
+      : charCode
+  );
+}
