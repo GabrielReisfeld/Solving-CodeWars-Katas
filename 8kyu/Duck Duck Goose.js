169d2619a93a7a7edf52540c24a841470d00f79e
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-12 13:27:18 -0300

    Solved "Duck Duck Goose"

--- 8kyu/Duck Duck Goose.js
+++ 8kyu/Duck Duck Goose.js
@@ -3,3 +3,7 @@ The objective of Duck, duck, goose is to walk in a circle, tapping on each playe
 
 Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
 */
+
+function duckDuckGoose(players, goose) {
+  return players[(goose - 1) % players.length].name;
+}
