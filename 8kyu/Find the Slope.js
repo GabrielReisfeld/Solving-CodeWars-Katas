b6c41a51eee3ff9e368c11c721633107b411979c
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-13 11:09:39 -0300

    Solved "Find the Slope"

--- 8kyu/Find the Slope.js
+++ 8kyu/Find the Slope.js
@@ -11,3 +11,8 @@ For an undefined slope (division by 0), return undefined . Note that the "undefi
 
 Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). Slope: https://en.wikipedia.org/wiki/Slope
 */
+
+function slope([x1, y1, x2, y2]) {
+  const result = (y2 - y1) / (x2 - x1);
+  return isFinite(result) ? `${result}` : "undefined";
+}
