363c232ea9f7ff6febd00a914e759c9aa5658d91
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-13 11:40:21 -0300

    Solved "Count the divisors of a number"

--- 7kyu/Count the divisors of a number.js
+++ 7kyu/Count the divisors of a number.js
@@ -9,3 +9,13 @@ divisors(5)  = 2  // 1, 5
 divisors(12) = 6  // 1, 2, 3, 4, 6, 12
 divisors(30) = 8  // 1, 2, 3, 5, 6, 10, 15, 30
 */
+
+function getDivisorsCnt(n) {
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      result += 1;
+    }
+  }
+  return result;
+}
