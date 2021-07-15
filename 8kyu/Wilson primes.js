d2eed5d2aaedb5ca2aeae8b21ff72cf7f51bc893
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-15 13:10:45 -0300

    Solved "Wilson primes"

--- 8kyu/Wilson primes.js
+++ 8kyu/Wilson primes.js
@@ -5,3 +5,7 @@ Then ((P-1)! + 1) / (P * P) should give a whole number.
 
 Your task is to create a function that returns true if the given number is a Wilson prime.
 */
+
+function amIWilson(p) {
+  return [5, 13, 563].indexOf(p) > -1;
+}
