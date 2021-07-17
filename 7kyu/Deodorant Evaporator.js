4b2dd06928550b07eeb1a21412fc7e3d67379daf
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-17 15:04:50 -0300

    Solved "Deodorant Evaporator"

--- 7kyu/Deodorant Evaporator.js
+++ 7kyu/Deodorant Evaporator.js
@@ -12,3 +12,13 @@ Note:
 
 Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
 */
+
+function evaporator(content, evap_per_day, threshold) {
+  let result = 0;
+  let percentage = 100;
+  while (percentage > threshold) {
+    percentage = percentage - percentage * (evap_per_day / 100);
+    result += 1;
+  }
+  return result;
+}
