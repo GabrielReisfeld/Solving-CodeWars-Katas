231ece6647b2dbf08a4d1f13f785afac4f9ab718
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-19 12:21:20 -0300

    Solved "Broken Counter"

--- 8kyu/Broken Counter.js
+++ 8kyu/Broken Counter.js
@@ -19,3 +19,20 @@ class Counter {
     this.value = 0;
   }
 }
+
+// Another Solution:
+//   function Counter() {
+//     this.value = 0;
+//   }
+
+//   Counter.prototype.increase = function() {
+//     this.value++;
+//   };
+
+//   Counter.prototype.getValue = function() {
+//     return this.value;
+//   };
+
+//   Counter.prototype.reset = function() {
+//     this.value = 0;
+//   };
