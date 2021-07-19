a698a2d9d1f55e278f7d22519d67feb0539b02bc
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-19 13:32:32 -0300

    Solved "Find the middle element"

--- 7kyu/Find the middle element.js
+++ 7kyu/Find the middle element.js
@@ -15,3 +15,7 @@ gimme([5, 10, 14]) => 1
 
 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
 */
+
+var gimme = function (inputArray) {
+  return [...inputArray].indexOf(inputArray.sort((a, b) => a - b)[1]);
+};
