bd930c07c0b8f5b7a24951cb50b7623430c7e86e
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-22 17:53:54 -0300

    Solved "Grasshopper - Terminal Game #1"

--- 8kyu/Grasshopper - Terminal Game #1.js
+++ 8kyu/Grasshopper - Terminal Game #1.js
@@ -25,3 +25,12 @@ class Hero {
     this.experience = experience;
   }
 }
+
+// Another Solution:
+// function Hero (name) {
+//     this.name = name || 'Hero';
+//     this.position = '00';
+//     this.health = 100;
+//     this.damage = 5;
+//     this.experience = 0;
+//   }
