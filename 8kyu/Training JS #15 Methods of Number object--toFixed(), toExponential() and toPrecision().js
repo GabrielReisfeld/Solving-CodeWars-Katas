bc841bbf74da72de73f14eea5bd0b0e99aec7770
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-29 15:10:22 -0300

    Solved "Training JS #15..."

--- 8kyu/Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision().js
+++ 8kyu/Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision().js
@@ -77,3 +77,7 @@ howManySmaller([1.234,1.235,1.228],1.24) should return 2
 howManySmaller([1.1888,1.1868,1.1838],1.19) should return 1
 howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2
 */
+
+function howManySmaller(arr, n) {
+  return arr.filter((num) => num.toFixed(2) < n).length;
+}
