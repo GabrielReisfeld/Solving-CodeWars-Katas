de98fe51b408d22a542c784abdfc07df0ff7cda8
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-29 15:08:29 -0300

    Solved "Training JS #13..."

--- 8kyu/Training JS #13 Number object and its properties.js
+++ 8kyu/Training JS #13 Number object and its properties.js
@@ -47,3 +47,20 @@ whatNumberIsIt(100) should return "Input number is 100"
 
 What you need to think about is how to judge it correctly and effectively and don't forget isNaN().
 */
+
+function whatNumberIsIt(n) {
+  switch (n) {
+    case Number.MAX_VALUE:
+      return "Input number is Number.MAX_VALUE";
+    case Number.MIN_VALUE:
+      return "Input number is Number.MIN_VALUE";
+    case Number.POSITIVE_INFINITY:
+      return "Input number is Number.POSITIVE_INFINITY";
+    case Number.NEGATIVE_INFINITY:
+      return "Input number is Number.NEGATIVE_INFINITY";
+    case Number(n):
+      return "Input number is " + n;
+    default:
+      return "Input number is Number.NaN";
+  }
+}
