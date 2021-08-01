21a4c30fd9f1288bc7722f6dec16abd454da191e
Gabriel Reisfeld <greisfeld@gmail.com>
2021-07-31 21:28:32 -0300

    Solved "Over The Road"

--- 7kyu/Over The Road.js
+++ 7kyu/Over The Road.js
@@ -25,3 +25,7 @@ If you are timing out, running out of memory, or get any kind of "error", read o
 
 To solve this, you need to think of a way to do the kata without making massive lists or huge for loops. Read the discourse for some inspiration :)
 */
+
+function overTheRoad(address, n) {
+  return n * 2 - address + 1;
+}
