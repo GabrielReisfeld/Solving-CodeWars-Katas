0cab1fa56f122da3e64936d1df9f05ccb6a05e14
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-02 15:57:16 -0300

    "Hello Happy Codevarrior!"

---
+++ 8kyu/Hello Happy Codevarrior!.js
@@ -0,0 +1,13 @@
+/*
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+
+*/
