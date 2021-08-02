264389b8ead6618ce70a087ca9c8de0dd976c141
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-02 13:11:39 -0300

    Solved "Type of sum"

--- 8kyu/Type of sum.js
+++ 8kyu/Type of sum.js
@@ -1,3 +1,8 @@
 /*
 Return the type of the sum of the two arguments
 */
+
+function typeOfSum(a, b) {
+  if (typeof (a + b) === "number") return "number";
+  if (typeof (a + b) === "string") return "string";
+}
