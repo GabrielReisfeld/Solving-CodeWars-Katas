6e152a7f3dab52ad391d2e401212c584745265e4
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-02 15:58:06 -0300

    Solved "Hello Happy Codevarrior!"

--- 8kyu/Hello Happy Codevarrior!.js
+++ 8kyu/Hello Happy Codevarrior!.js
@@ -11,3 +11,15 @@ var boris  = new Warrior("Boris")
 albert.toString() --> "Hi! my name's Boris" <-- ohoh..
 
 */
+
+function Warrior(n) {
+  var name = n;
+  this.name = function (n) {
+    if (n) name = n;
+    return name;
+  };
+}
+
+Warrior.prototype.toString = function () {
+  return "Hi! my name's " + this.name();
+};
