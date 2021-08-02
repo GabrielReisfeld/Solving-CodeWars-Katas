821bc8a3e945c6c7baa51cca9af2d8323b8e728d
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-02 13:20:20 -0300

    Solved "Fun with ES6 Classes #1 - People, people"

--- 8kyu/Fun with ES6 Classes #1 - People, people, people.js
+++ 8kyu/Fun with ES6 Classes #1 - People, people, people.js
@@ -13,3 +13,18 @@ You may use any valid syntax you like; however, it is highly recommended that yo
 
 Have fun! :D
 */
+
+class Person {
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.age = age,
+    this.gender = gender;
+  }
+  sayFullName() {
+    return this.firstName + " " + this.lastName;
+  }
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+}
