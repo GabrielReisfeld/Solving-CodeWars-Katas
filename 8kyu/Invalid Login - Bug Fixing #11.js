f10ebaed3f4b76f75160ed42edfc988adb05dad1
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-03 15:20:23 -0300

    Solved "Invalid Login - Bug Fixing #11"

--- 8kyu/Invalid Login - Bug Fixing #11.js
+++ 8kyu/Invalid Login - Bug Fixing #11.js
@@ -13,3 +13,11 @@ Usage
 var database = new Database();
 database.login('Timmy', 'password');
 */
+
+function validate(username, password) {
+  if (password.includes("||") || password.includes("//")) {
+    return "Wrong username or password!";
+  }
+  var database = new Database();
+  return database.login(username, password);
+}
