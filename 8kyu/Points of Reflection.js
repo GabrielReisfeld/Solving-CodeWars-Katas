6a92eefec664bf60965409941a98931b1d40b8e3
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-04 09:11:35 -0300

    Solved "Points of Reflection"

--- 8kyu/Points of Reflection.js
+++ 8kyu/Points of Reflection.js
@@ -4,3 +4,7 @@ Task
 
 Given two points P and Q, output the symmetric point of point P about Q. Each argument is a two-element array of integers representing the point's X and Y coordinates. Output should be in the same format, giving the X and Y coordinates of point P1. You do not have to validate the input.
 */
+
+function symmetricPoint(p, q) {
+  return [q[0] * 2 - p[0], q[1] * 2 - p[1]];
+}
