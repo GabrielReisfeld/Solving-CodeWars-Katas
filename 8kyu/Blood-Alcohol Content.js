0148ee3706aa2810315f99b1a6be57f13cda7a9d
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-06 15:28:05 -0300

    Solved "Blood-Alcohol Content"

--- 8kyu/Blood-Alcohol Content.js
+++ 8kyu/Blood-Alcohol Content.js
@@ -24,3 +24,7 @@ Output must be returned as a number data-type, greater than or equal to 0, with 
 
 Using these parameters, create the function that will calculate Bob's and other partier's BAC.
 */
+
+function bloodAlcoholContent(drinks, weight, sex, time){
+    return +(drinks.ounces * drinks.abv * 5.14 / weight * (sex === 'male' ? 0.73 : 0.66) - 0.015 * time).toFixed(4);
+  }
