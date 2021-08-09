055acb57e0b313872ee5f353b5852a0e078d0076
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-08 23:07:31 -0300

    Solved "Caffeine Script"

--- 7kyu/Caffeine Script.js
+++ 7kyu/Caffeine Script.js
@@ -14,3 +14,10 @@ caffeineBuzz(3)   => "Java"
 caffeineBuzz(6)   => "JavaScript"
 caffeineBuzz(12)  => "CoffeeScript"
 */
+
+function caffeineBuzz(n) {
+  if (n % 12 === 0) return "CoffeeScript";
+  if (n % 6 === 0) return "JavaScript";
+  if (n % 3 === 0) return "Java";
+  return "mocha_missing!";
+}
